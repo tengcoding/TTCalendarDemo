68fd775f8095227fad4de1391c874fa4069e8a9e
TT_dashen <ltt143@126.com>
2016-07-06 18:32:53 +0800

    first

---
+++ CalendarDemo/CalendarDemo/TTCalendarView.h
@@ -0,0 +1,13 @@
+//
+//  TTCalendarView.h
+//  CalendarDemo
+//
+//  Created by 梁腾 on 16/7/6.
+//  Copyright © 2016年 TT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTCalendarView : UIView
+
+@end
---
+++ CalendarDemo/CalendarDemo/TTCalendarView.m
@@ -0,0 +1,26 @@
+//
+//  TTCalendarView.m
+//  CalendarDemo
+//
+//  Created by 梁腾 on 16/7/6.
+//  Copyright © 2016年 TT. All rights reserved.
+//
+
+#import "TTCalendarView.h"
+
+@interface TTCalendarView ()
+/** 顶部的scrollView */
+@property (nonatomic,weak) UIScrollView * scrollView;
+/** 联动的view */
+@property (nonatomic,weak) UIView *centerView;
+
+
+
+
+@end
+
+@implementation TTCalendarView
+
+
+
+@end
