40ec075f3c685b47661fef7d4605e2a594189c9c
TT_dashen <ltt143@126.com>
2016-07-06 17:34:24 +0800

    project

---
+++ CalendarDemo/CalendarDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CalendarDemo
+//
+//  Created by 梁腾 on 16/7/6.
+//  Copyright © 2016年 TT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
